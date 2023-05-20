67112b834a0d26fa87901420efadd2da7e2c602a
Alvin Haruna <harunaalvin69@gmail.com>
2023-05-20 19:07:23 +0100

    created dictionary to store candidate & vote

---
+++ app.py
@@ -0,0 +1,19 @@
+# Create dictionary(poll) to store Candidates & Vote count starting at 0
+
+poll = {
+    "APC": {
+        "Karen Joy": 0,
+    },
+    "PDP": {
+        "": 0,
+    },
+    "APGA": {
+        "Ben White": 0,
+    },
+    "AD": {
+        "John Doe": 0,
+    },
+    "LP": {
+        "Peter Obi": 0,
+    }
+}
