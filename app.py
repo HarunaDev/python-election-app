3a54aeb8ecbe0e1fc05ed5196e1d362188f83566
Alvin Haruna <harunaalvin69@gmail.com>
2023-05-21 01:23:10 +0100

    added comments and refactored code

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
-# Create dictionary(poll) to store Candidates & Vote count starting at 0
-
-poll = {
-    "APC": {
-        "Karen Joy": 0,
-    },
-    "PDP": {
-        "": 0,
-    },
-    "APGA": {
-        "Ben White": 0,
-    },
-    "AD": {
-        "John Doe": 0,
-    },
-    "LP": {
-        "Peter Obi": 0,
-    }
-}
+import modules.polling as polling, modules.voting as voting
 
 # get registered voters
-getRegisteredVoters = int(input("Enter number of registered voters"))
+getRegisteredVoters = int(input("Enter number of registered voters: "))
 
-# Display Party to voters
-parties = poll.keys()
-print("Vote a running party\n")
-for party in parties:
-    print(party)
-
-# Get vote choice from voters
-vote = input("Choose your party: ")
-
+# count votes
+vote_count = 0 #initialise vote_count variable to track votes
+while vote_count < getRegisteredVoters:
+    # call get_votes method from voting module and update count variable
+    voting.get_votes()
+    vote_count = vote_count + 1
