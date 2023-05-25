050f7d801cb57becee8f728f7c75c124c8fba091
Alvin Haruna <harunaalvin69@gmail.com>
2023-05-26 00:25:17 +0100

    put code to get candidate with highest votes in poll into a try catch block to prevent crash
    bug in code on line 14

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ while vote_count < getRegisteredVoters:
     # call get_votes method from voting module and update count variable
     voting.get_votes()
     vote_count = vote_count + 1
+    result = polling.poll
+    try:
+        winner = max(result.items(), key=lambda x: x[1])
+    except Exception as e:
+        print(f"{e}")
+    else:
+        candidate = winner[0]
+        no_of_votes = winner[1]
+        print(f"The winner of the election is {candidate} with {no_of_votes} votes.")
+        print("no bug here")
+    finally:
+        print("program ended")
